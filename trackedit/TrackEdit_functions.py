cb2eef4833f40cb740484bcece85b428ae6480e2
TeunHuijben <teun.huijben@czbiohub.org>
2025-02-21 14:28:10 -0800

    fixed duplication logic + fixed DatabaseArray getting at t=0 non refresh

--- trackedit/TrackEdit_functions.py
+++ trackedit/TrackEdit_functions.py
@@ -227,11 +227,14 @@ class TrackEditClass():
         if add_flag:
             selected = get_node_values(self.databasehandler.config_adjusted.data_config, node_id, NodeDB.selected)
             time_original = get_node_values(self.databasehandler.config_adjusted.data_config, node_id, NodeDB.t)
-            if (selected == 1 & time_original == time):
+            if (selected == True) or (time_original == time):
                 add_flag = False
-                show_warning(f"Cell is already in solution at time {time}")
                 self.EditingMenu.duplicate_cell_id_input.setText('')
                 self.EditingMenu.duplicate_time_input.setText('')
+                if selected == True:
+                    show_warning(f"Cell is already in solution at this time {time}")
+                if time_original == time:
+                    show_warning(f"Cell is from this time point, use 'Add Cell' field above to add this cell, it is not a duplication")
 
         if add_flag:
             #move to the respective chunk of the added cell
