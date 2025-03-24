78311cd916be00377948de48f22bfa5a2d06566b
Teun Huijben <45037215+TeunHuijben@users.noreply.github.com>
2025-03-24 15:31:54 -0700

    Update demo_neuromast.py

--- scripts/demo_neuromast.py
+++ scripts/demo_neuromast.py
@@ -39,7 +39,7 @@ db_filename_old = "data.db"  # name of the database file to start from
 Tmax = 600  # maximum number of frames display (crop the data to this number of frames)
 scale = (2.31, 1, 1)  # (Z),Y,X
 layer_name = "ultrack"  # name of the layer in napari
-allow_overwrite = True  # overwrite existing database/changelog
+allow_overwrite = False  # overwrite existing database/changelog
 
 # OPTIONAL: imaging data
 # imaging_zarr_file = "/hpc/projects/jacobo_group/iSim_processed_files/steady_state_timelapses/20241003_2dpf_myo6b_bactin_GFP_she_h2b_gfp_cldnb_lyn_mScarlet/46hpf_fish1_1/2_deconvolution_and_registration/deconvolved_and_registered_corrected_cropped850.zarr"
