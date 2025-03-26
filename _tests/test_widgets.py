283e1ffae59fe013d5b69c4dacfe80077c0ce690
TeunHuijben <teun.huijben@czbiohub.org>
2025-03-26 15:16:58 -0700

    test n=5

--- _tests/test_widgets.py
+++ _tests/test_widgets.py
@@ -3,8 +3,6 @@ from typing import Callable
 
 import napari
 import pytest
-from qtpy.QtCore import QTimer
-from qtpy.QtWidgets import QApplication, QPushButton
 from ultrack.config import MainConfig
 from ultrack.utils.test_utils import (  # noqa: F401, F811
     config_content,
@@ -32,10 +30,10 @@ def viewer_and_trackedit(
     track_edit = run_trackedit(
         working_directory=working_directory,
         db_filename="data.db",
-        tmax=20,
+        tmax=5,
         scale=(1, 1, 1),
         allow_overwrite=True,
-        time_chunk_length=2,
+        time_chunk_length=3,
         time_chunk_overlap=1,
         imaging_zarr_file="",
         imaging_channel="",
@@ -48,7 +46,7 @@ def viewer_and_trackedit(
 @pytest.mark.parametrize(
     "timelapse_mock_data",  # noqa: F811
     [
-        {"length": 3, "size": 64, "n_dim": 3},
+        {"length": 5, "size": 64, "n_dim": 3},
     ],
     indirect=True,
 )
@@ -58,31 +56,31 @@ def test_trackedit_widgets(
     """Test UI interactions with the viewer and widgets"""
     viewer, track_edit = viewer_and_trackedit
 
-    # # Get the NavigationWidget directly from TrackEdit instance
-    # navigation_widget = track_edit.NavigationWidget
-    # assert navigation_widget is not None, "NavigationWidget not found"
+    # Get the NavigationWidget directly from TrackEdit instance
+    navigation_widget = track_edit.NavigationWidget
+    assert navigation_widget is not None, "NavigationWidget not found"
 
-    # editing_menu = track_edit.EditingMenu
-    # assert editing_menu is not None, "EditingMenu not found"
+    editing_menu = track_edit.EditingMenu
+    assert editing_menu is not None, "EditingMenu not found"
 
-    # # Get boxes through the properly connected NavigationWidget
-    # time_box = navigation_widget.time_box
-    # assert time_box is not None, "TimeBox not found"
+    # Get boxes through the properly connected NavigationWidget
+    time_box = navigation_widget.time_box
+    assert time_box is not None, "TimeBox not found"
 
-    # red_flag_box = navigation_widget.red_flag_box
-    # assert red_flag_box is not None, "RedFlagBox not found"
+    red_flag_box = navigation_widget.red_flag_box
+    assert red_flag_box is not None, "RedFlagBox not found"
 
-    # division_box = navigation_widget.division_box
-    # assert division_box is not None, "DivisionBox not found"
+    division_box = navigation_widget.division_box
+    assert division_box is not None, "DivisionBox not found"
 
-    # toAnnotateBox = track_edit.AnnotationWidget.todo_box
-    # assert toAnnotateBox is not None, "ToAnnotateBox not found"
+    toAnnotateBox = track_edit.AnnotationWidget.todo_box
+    assert toAnnotateBox is not None, "ToAnnotateBox not found"
 
-    # TV = track_edit.tracksviewer
+    TV = track_edit.tracksviewer
 
-    # check_selection(TV)
-    # check_time_box(time_box)
-    # check_editing(TV, editing_menu)
+    check_selection(TV)
+    check_time_box(time_box)
+    check_editing(TV, editing_menu)
     # check_red_flag_box(TV, red_flag_box)
     # check_division_box(division_box)
     # check_annotation(toAnnotateBox)
@@ -116,10 +114,10 @@ def check_selection(TV):
 def check_time_box(time_box):
     """Check time box functionality"""
 
-    time_box.time_input.setText(str(19))
+    time_box.time_input.setText(str(4))
     time_box.on_time_input_entered()
 
-    time_box.time_input.setText(str(2))
+    time_box.time_input.setText(str(0))
     time_box.on_time_input_entered()
 
     time_box.press_next()
@@ -129,42 +127,42 @@ def check_time_box(time_box):
 def check_editing(TV, editing_menu):
     """Check track editing functionality"""
     # Test: delete cell and undo
-    TV.selected_nodes.add(3000009, append=False)
-    TV.delete_node()
-    TV.undo()
+    # TV.selected_nodes.add(3000009, append=False)
+    # TV.delete_node()
+    # TV.undo()
 
-    # # Test: redo and undo deletion
-    TV.redo()
-    TV.undo()
+    # # # Test: redo and undo deletion
+    # TV.redo()
+    # TV.undo()
 
-    # Function to handle dialog
-    def handle_dialog():
-        print("handling dialog")
-        for widget in QApplication.topLevelWidgets():
-            if widget.windowTitle() == "Delete existing edge?":
-                for button in widget.findChildren(QPushButton):
-                    if button.text() == "&OK":
-                        button.click()
-                        break
+    # # Function to handle dialog
+    # def handle_dialog():
+    #     print("handling dialog")
+    #     for widget in QApplication.topLevelWidgets():
+    #         if widget.windowTitle() == "Delete existing edge?":
+    #             for button in widget.findChildren(QPushButton):
+    #                 if button.text() == "&OK":
+    #                     button.click()
+    #                     break
 
-    # # Test: break/add edge
-    TV.selected_nodes.add(2000009, append=False)
-    TV.selected_nodes.add(3000010, append=True)
-    QTimer.singleShot(100, handle_dialog)  # handle popup window
-    TV.create_edge()
-    print("dialog correctly handled")
+    # # # Test: break/add edge
+    # TV.selected_nodes.add(2000009, append=False)
+    # TV.selected_nodes.add(3000010, append=True)
+    # QTimer.singleShot(100, handle_dialog)  # handle popup window
+    # TV.create_edge()
+    # print("dialog correctly handled")
 
-    # # Test: add node
-    editing_menu.click_on_hierarchy_cell(3000012)
-    editing_menu.add_cell_from_button()
-    TV.undo()
+    # # # Test: add node
+    # editing_menu.click_on_hierarchy_cell(3000012)
+    # editing_menu.add_cell_from_button()
+    # TV.undo()
 
-    # # Test: duplicate node
-    editing_menu.click_on_hierarchy_cell(3000012)
-    editing_menu.duplicate_cell_id_input.setText(str(3000012))
-    editing_menu.duplicate_time_input.setText(str(1))
-    editing_menu.duplicate_cell_from_button()
-    TV.undo()
+    # # # Test: duplicate node
+    # editing_menu.click_on_hierarchy_cell(3000012)
+    # editing_menu.duplicate_cell_id_input.setText(str(3000012))
+    # editing_menu.duplicate_time_input.setText(str(1))
+    # editing_menu.duplicate_cell_from_button()
+    # TV.undo()
 
 
 def check_red_flag_box(TV, red_flag_box):
