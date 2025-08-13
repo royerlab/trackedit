b5a1714ccf1c04ae81ae10522a40a2545fb9a24e
TeunHuijben <teun.huijben@czbiohub.org>
2025-08-13 14:47:57 -0700

    revert neuromast path back to default

--- scripts/script_neuromast.py
+++ scripts/script_neuromast.py
@@ -14,8 +14,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, message=".*qt_viewer.*
 # **********INPUTS*********
 # path to the working directory that contains the database file AND metadata.toml:
 working_directory = Path(
-    # "/hpc/projects/group.royer/people/teun.huijben/data/Akila/trackedit_example_data/"
-    "/home/teun.huijben/Documents/data/Akila/trackedit_example_data/"
+    "/hpc/projects/group.royer/people/teun.huijben/data/Akila/trackedit_example_data/"
 )
 # name of the database file to start from, or "latest" to start from the latest version, defaults to "data.db"
 db_filename_start = "latest"
